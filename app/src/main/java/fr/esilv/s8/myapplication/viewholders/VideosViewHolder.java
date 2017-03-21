02c83eeb6e736520d174ad54cafd696e5cf05a94
sdfr <dufour.sarah@hotmail.fr>
2017-03-21 23:23:41 +0100

    a

--- app/src/main/java/fr/esilv/s8/myapplication/viewholders/VideosViewHolder.java
+++ app/src/main/java/fr/esilv/s8/myapplication/viewholders/VideosViewHolder.java
@@ -11,17 +11,25 @@ import fr.esilv.s8.myapplication.models.Videos;
 public class VideosViewHolder extends RecyclerView.ViewHolder {
 
     private TextView title;
-    private TextView url_address;
+    private TextView description;
+    private TextView publicationDate;
+    private TextView author;
+
     private OnVideoSelectedListener onVideoSelectedListener;
 
     public VideosViewHolder(View itemView) {
         super(itemView);
         title = (TextView) itemView.findViewById(R.id.title);
-        url_address = (TextView) itemView.findViewById(R.id.url_address);
+        description = (TextView) itemView.findViewById(R.id.description);
+        publicationDate = (TextView) itemView.findViewById(R.id.publicationDate);
+        author = (TextView) itemView.findViewById(R.id.channel);
     }
 
     public void bind(final Videos.ItemsBean video) {
         title.setText(video.getSnippet().getTitle());
+        description.setText(video.getSnippet().getDescription());
+        publicationDate.setText(video.getSnippet().getPublishedAt());
+        author.setText(video.getSnippet().getChannelTitle());
         itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
